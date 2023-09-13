62db39a75c012834dd49603d28b483447d8109c1
carlos padilla <coheling@gmail.com>
2023-09-13 15:18:51 -0600

    investigacion de las variables

---
+++ Investigacion.py
@@ -0,0 +1,4 @@
+#Las variables se definen en cadena, entero y flotente.
+#Los enteros no tienen límite teórico, están limitados por la memoria disponible.
+#Los flotantes utilizan representación de punto y tienen límites de precisión.
+#La suma de los primeros n números pares es n = 5, osea suma_pares = n * (n + 1).
